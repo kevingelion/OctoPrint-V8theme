3a821b88b4f638223d19672a7258c9bfa1692982
Jack Minardi <jack@minardi.org>
2015-07-27 00:26:22 -0400

    fix autoupdate

--- octoprint_v8theme/__init__.py
+++ octoprint_v8theme/__init__.py
@@ -1,13 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class V8themePlugin(octoprint.plugin.AssetPlugin):
@@ -18,15 +11,10 @@ class V8themePlugin(octoprint.plugin.AssetPlugin):
             #less=['less/my_styles.less']
         )
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "V8theme Plugin"
-
 
 def get_update_information(self, *args, **kwargs):
     return dict(
-        v8theme_plugin=dict(
+        v8theme=dict(
             type="github_commit",
             user="Voxel8",
             repo="OctoPrint-V8theme",
@@ -37,6 +25,7 @@ def get_update_information(self, *args, **kwargs):
 
 def __plugin_load__():
     global __plugin_implementation__
+    global __plugin_hooks__
     __plugin_implementation__ = V8themePlugin()
 
     __plugin_hooks__ = {

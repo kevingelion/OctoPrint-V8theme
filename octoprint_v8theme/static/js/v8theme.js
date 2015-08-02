eb59d7a62f7be1eaf3dc951537a6b19cf1a56319
Jack Minardi <jack@minardi.org>
2015-08-01 20:02:06 -0400

    use proper viewmodel

--- octoprint_v8theme/static/js/v8theme.js
+++ octoprint_v8theme/static/js/v8theme.js
@@ -1,7 +1,18 @@
-$(window).load(function(){
-    /* combine temp and control tabs */
-    $("#temp_link").remove();
-    $("#control_link").addClass("active");
-    $("#control").prepend($("#temp").contents());
-    $("#control").addClass("active");
+$(function() {
+    function V8ThemeViewModel(parameters) {
+        var self = this;
+
+        self.onAllBound = function() {
+		$("#temp_link").remove();
+		$("#control_link").addClass("active");
+		$("#control").prepend($("#temp").contents());
+		$("#control").addClass("active");
+	};
+    }
+
+    OCTOPRINT_VIEWMODELS.push([
+        V8ThemeViewModel,
+        [],
+        ""
+    ]);
 });

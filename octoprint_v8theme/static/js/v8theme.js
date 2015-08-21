19ee2a120084b8784d3f4e8660a577aaa3f17d85
Jack Minardi <jack@minardi.org>
2015-08-21 00:48:34 -0400

    update plot when control tab selected

--- octoprint_v8theme/static/js/v8theme.js
+++ octoprint_v8theme/static/js/v8theme.js
@@ -1,21 +1,27 @@
 $(function() {
     function V8ThemeViewModel(parameters) {
         var self = this;
+        self.temp = parameters[0]
 
         self.onAllBound = function() {
-            // Everything is added to the Temperature tab because the graph is
-            // wired to redraw when it is selected.
-            $("#control_link").remove();
-            $("#temp_link").addClass("active");
-            //$("#temp_link").text("Control");
-            $("#temp").append($("#control").contents());
-            $("#temp").addClass("active");
+            $("#temp_link").remove();
+            $("#control_link").addClass("active");
+            $("#control").prepend($("#temp").contents());
+            $("#control").addClass("active");
         };
+
+        self.onAfterTabChange = function(current, previous) {
+            if (current != "#control") {
+                return;
+            }
+            self.temp.updatePlot();
+        }
+
     }
 
     OCTOPRINT_VIEWMODELS.push([
         V8ThemeViewModel,
-        [],
+        ["temperatureViewModel"],
         ""
     ]);
 });

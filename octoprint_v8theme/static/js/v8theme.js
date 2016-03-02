b32c38b88e7155ff8b44787c8b442d9ea61c9ccd
Kevin Murphy <murphyk3@wit.edu>
2016-03-01 19:39:19 -0500

    Move custom homing command to control view model

--- octoprint_v8theme/static/js/v8theme.js
+++ octoprint_v8theme/static/js/v8theme.js
@@ -137,10 +137,16 @@ $(function() {
       if (typeof self.temperature.plot !== "undefined") self.temperature.plot.unhighlight();
     }
 
+    /* Modified from OctoPrint
+     * Reason: Change homing button to run G28 & G29 instead of built in homing function
+     */
     self.onBeforeBinding = function () {
       $("#customControls_containerTemplate_collapsable, #customControls_containerTemplate_nameless").html(function() {
         return $(this).html().replace(/"custom_section">/g, '"custom_section" data-bind="css: { plugin_control: (plugin_control) }">');
       });
+    };
+
+    self.control.onBeforeBinding = function () {
       $("#control-xyhome").attr("data-bind", function() {
         return $(this).attr("data-bind").replace(/sendHomeCommand\(\[\'x\', \'y\'\]\)/g, "sendCustomCommand({type:'commands',commands:['G28', 'G29']})");
       });
